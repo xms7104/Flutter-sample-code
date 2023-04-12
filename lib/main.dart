5d7ea48d994bb7ec889bd52d80dcc55e204cbc3d
xms7104 <minsan.x@doomeye.com>
2023-04-12 18:02:25 +0800

    fetch api test

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  getData();
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
